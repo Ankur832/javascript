bac3d9876b9243f98d8e74a24e58de87eb41a614
Ankur832 <pandeyankur114@gmail.com>
2025-07-01 10:25:11 -0700

    Add files via upload
    1/7/2025

---
+++ script1.js
@@ -0,0 +1,28 @@
+//Math
+
+console.log(Math)
+//abs conver the only negative to positive
+console.log(Math.abs(-4));
+
+// for round off the value
+console.log(Math.round(2.94))
+// we also use the ceil and floor
+//in ceil it must the higher value
+console.log(Math.ceil(4.1))
+// for floor we use the must lower value
+console.log(Math.floor(3.1))
+
+///NUMBERS 
+
+const number= new Number('100')
+//for change the number to string 
+ const abc =console.log(number.toString());
+ console.log(abc)
+
+ const otherNumber=123.874;
+ console.log(otherNumber.toPrecision(3))
+
+ //for read the numbers easily we use tolocalstring
+
+ const thousand= 10000000;
+ console.log(thousand.toLocaleString('en-IN'))
